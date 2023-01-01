b7d3817370f6f44fe0f6eff0980ccef942bceb69
affandi26 <affandiahmad351@gmail.com>
2023-01-01 18:55:30 +0700

    add view.php

--- pages/kontak/view.php
+++ pages/kontak/view.php
@@ -19,7 +19,7 @@
                 <div style="padding: 0 10px;text-align:justify"> 
                     <div class="panel panel-default">
                         <div class="panel-body">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6379576006807!2d106.7896683143111!3d-6.311201663517896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ee3d24a8fb37%3A0x924263851ab0143f!2sWarbo+Cafe!5e0!3m2!1sid!2sid!4v1555743489813!5m2!1sid!2sid" width="1087" height="350" frameborder="0" style="border:0" width="1087" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.430876654189!2d109.23158492411818!3d-7.417472997474377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e655e607976747f%3A0xe08b0a6c0f2506e2!2sJalan%20Dokter%20Angka%20Gang%20III%20No.28%2C%20Karangjengkol%2C%20Sokanegara%2C%20Kec.%20Purwokerto%20Tim.%2C%20Kabupaten%20Banyumas%2C%20Jawa%20Tengah%2053115!5e0!3m2!1sid!2sid!4v1672570369686!5m2!1sid!2sid" width="1087" height="350" frameborder="0" style="border:0" width="1087" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
@@ -30,8 +30,8 @@
             <div class="col-lg-12">
                 <div style="padding: 0 10px;text-align:justify"> 
                     <h4>Kantor</h4>
-                    <p><i style="margin-right:10px" class="fa fa-map-marker"></i>Jalan H. Ipin, RT. 8/ RW. 1, Kel, RT.7/RW.1, Pd. Labu, Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12450</p>
-                    <p><i style="margin-right:10px" class="fa fa-phone"></i>+62 821 5480 5956</p>
+                    <p><i style="margin-right:10px" class="fa fa-map-marker"></i>Jalan Dokter Angka Gang III No.23, Karangjengkol, Sokanegara, Kec. Purwokerto Tim., Kabupaten Banyumas, Jawa Tengah 53115</p>
+                    <p><i style="margin-right:10px" class="fa fa-phone"></i>+62 819 1128 9023</p>
                 </div>
             </div>
         </div>
